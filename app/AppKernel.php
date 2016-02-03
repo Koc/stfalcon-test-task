611b76af11cd5e7d1d26d49c5b41ff8ba452fa54
Konstantin.Myakshin <koc-dp@yandex.ru>
2016-02-03 18:54:35 +0200

    Install JMSSerializer bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
 
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
             new Vich\UploaderBundle\VichUploaderBundle(),
